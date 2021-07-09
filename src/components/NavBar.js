998953058040321f3d927cc4f25cc011b44725e2
OmkarBhede <omkarbhede@gamil.com>
2021-07-09 10:36:40 +0530

    name added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ export default class NavBar extends Component {
     return (
       <nav class="navbar navbar-light bg-light">
         <span class="navbar-brand mb-0 h1">Navbar</span>
+        <span>Omkar bhede</span>
       </nav>
     );
   }
